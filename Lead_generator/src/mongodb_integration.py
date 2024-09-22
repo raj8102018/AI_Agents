3ea9871306070b51530008053e5f45c96bee462e
raj8102018 <kunamallarajendraprasad@gmail.com>
2024-09-22 23:17:08 +0530

    test&src_modules error

--- Lead_generator/src/mongodb_integration.py
+++ Lead_generator/src/mongodb_integration.py
@@ -1,7 +1,12 @@
 # In mongodb_integration.py
 from pymongo import MongoClient
+from pymongo import UpdateOne
 import sys
 import os
+import random
+import time
+from bson.objectid import ObjectId
+
 
 # Add the root directory to the Python path to access 'config'
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -29,20 +34,92 @@ def fetch_leads():
     leads_list = list(leads)
     return leads_list
 
-# Update leads in the MongoDB collection with 'lead_type' and 'outbound message'
-def update_leads(lead):
+def update_leads(batch):
     leads_collection = connect_to_mongodb()
-        # Update the document in the database with the new key-value pairs
-    leads_collection.update_one(
-        {'_id': lead['_id']},  # Match by the lead's unique ID
-        {'$set': {'lead_type': lead['lead_type'], 'outbound message': lead['outbound message']}}
-    )
-    #print(f"Updated lead: {lead['_id']} with lead_type: {lead['lead_type']} and outbound message: {lead['outbound message']}")
+
+    # Create a list to hold update operations
+    operations = []
+
+    # Prepare the update operations for each lead
+    for lead in batch:
+        operation = UpdateOne(
+            {'_id': lead['_id']},  # Filter to match the lead's unique ID
+            {'$set': {
+                'lead_type': lead['lead_type'],  # Set the lead_type field
+                'outbound message': lead['outbound message']  # Set the outbound message field
+            }}
+        )
+        operations.append(operation)  # Add the operation to the list
+
+    # Perform the bulk write operation
+    if operations:  # Check if there are operations to execute
+        result = leads_collection.bulk_write(operations)
+
+    return result  # Return the result of the bulk write operation
+
+#Creating random leads based on list of industries and job titles available
+Industries  = ['IT Services And IT Consulting', 'Software Development', 
+            'Medical Device', 'Pharmaceuticals', 'Business Consulting And Services',
+            'Hospitals And Health Care','Health, Wellness & Fitness', 'Nonprofit Organizations', 
+            'Government Administration', 'Manufacturing', 
+            'Appliances, Electrical, And Electronics Manufacturing','Retail',  'Environmental Services', 'Design Services', 
+            'Wholesale']
+Job_titles  = ['Chief Executive Officer', 'President', 'Founder', 
+            'Vice President', 'Director','Executive Director', 'Managing Director', 'Owner', 
+            'General Manager', 'Vice President of Operations','CoOwner', 'Deputy Director', 'Executive Vice President']
+count = 20 #limiting the test size to 20
+
+new_lead_list = []
+for i in range(count):
+    ind_index = random.randint(0,len(Industries)-1)
+    job_index = random.randint(0,len(Job_titles)-1)
+    new_lead = {
+        'Company': f'test#{i+1}',
+        'Website': f'test#{i+1}',
+        'Industry': Industries[ind_index],
+        'Address Street': f'test#{i+1}',
+        'City': f'test#{i+1}',
+        'State': f'test#{i+1}',
+        'Zip Code': f'test#{i+1}',
+        'Country': f'test#{i+1}',
+        'Contact Number': f'test#{i+1}',
+        'First Name': f'test#{i+1}',
+        'Last Name': f'test#{i+1}',
+        'Job Title': Job_titles[job_index],
+        'Email': f'test#{i+1}@example.com',
+        'Linkedin URL': f'test#{i+1}'
+        }
+    new_lead_list.append(new_lead)
+
+# Function to add a new lead to the database
+def add_new_leads(leads_list):
+    leads_collection = connect_to_mongodb()
+    
+    # Insert the list of new leads into the 'leads' collection
+    result = leads_collection.insert_many(leads_list)
+    
+    # Print the IDs of the inserted documents
+    return result.inserted_ids
+
+def delete_leads(lead_id_list):
+    leads_collection = connect_to_mongodb()
+    
+    # Convert all lead IDs to ObjectId format
+    object_id_list = [ObjectId(lead_id) for lead_id in lead_id_list]
+    
+    # Delete the leads with the specified IDs using $in operator
+    result = leads_collection.delete_many({'_id': {'$in': object_id_list}})
+    
+    # Check how many documents were deleted
+    if result.deleted_count > 0:
+        print(f"{result.deleted_count} leads have been deleted.")
+    else:
+        print("No leads found with the specified IDs.")
+
+
+leads_n = add_new_leads(new_lead_list)
+# time.sleep(30)
+# delete_leads(leads_n)
 
 
 
-# # Example usage
-# if __name__ == "__main__":
-#     leads = fetch_leads()
-#     for lead in leads:
-#         print(lead)
--- Lead_generator/tests/test_response_generation.py
+++ Lead_generator/tests/test_response_generation.py
@@ -1,11 +1,17 @@
 import os
 import sys
+import time
+import random
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from transformers import BertTokenizer, TFBertForSequenceClassification, AdamWeightDecay
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from config.settings import MONGODB_URI, MONGODB_DB
+from src.response_generation import batch_processing, get_batches
+from src.lead_classification import lead_classification_update
+from src.mongodb_integration import update_leads
 
 # Function to connect to MongoDB
 def connect_to_mongodb():
@@ -14,53 +20,82 @@ def connect_to_mongodb():
     leads_collection = db['leads']
     return leads_collection
 
+#Creating random leads based on list of industries and job titles available
+Industries  = ['IT Services And IT Consulting', 'Software Development', 
+            'Medical Device', 'Pharmaceuticals', 'Business Consulting And Services',
+            'Hospitals And Health Care','Health, Wellness & Fitness', 'Nonprofit Organizations', 
+            'Government Administration', 'Manufacturing', 
+            'Appliances, Electrical, And Electronics Manufacturing','Retail',  'Environmental Services', 'Design Services', 
+            'Wholesale']
+Job_titles  = ['Chief Executive Officer', 'President', 'Founder', 
+            'Vice President', 'Director','Executive Director', 'Managing Director', 'Owner', 
+            'General Manager', 'Vice President of Operations','CoOwner', 'Deputy Director', 'Executive Vice President']
+count = 20 #limiting the test size to 20
+new_lead_list = []
+for i in range(count):
+    ind_index = random.randint(0,len(Industries)-1)
+    job_index = random.randint(0,len(Job_titles)-1)
+    new_lead = {
+        'Company': f'test#{i+1}',
+        'Website': f'test#{i+1}',
+        'Industry': Industries[ind_index],
+        'Address Street': f'test#{i+1}',
+        'City': f'test#{i+1}',
+        'State': f'test#{i+1}',
+        'Zip Code': f'test#{i+1}',
+        'Country': f'test#{i+1}',
+        'Contact Number': f'test#{i+1}',
+        'First Name': f'test#{i+1}',
+        'Last Name': f'test#{i+1}',
+        'Job Title': Job_titles[job_index],
+        'Email': f'test#{i+1}@example.com',
+        'Linkedin URL': f'test#{i+1}'
+        }
+    new_lead_list.append(new_lead)
+
 # Function to add a new lead to the database
-def add_new_lead(new_lead):
+def add_new_leads(leads_list):
     leads_collection = connect_to_mongodb()
     
-    # Insert the new lead into the 'leads' collection
-    result = leads_collection.insert_one(new_lead)
+    # Insert the list of new leads into the 'leads' collection
+    result = leads_collection.insert_many(leads_list)
     
-    # Print the ID of the inserted document
-    print(f"New lead added with ID: {result.inserted_id}")
+    # Print the IDs of the inserted documents
+    return result.inserted_ids
 
-# Example new lead document to add
-new_lead = {
-    'Company': 'New Company',
-    'Website': 'newcompany.com',
-    'Industry': 'Technology',
-    'Address Street': '123 New Street',
-    'City': 'New York',
-    'State': 'New York',
-    'Zip Code': 10001,
-    'Country': 'United States',
-    'Contact Number': '1 212 555 1234',
-    'First Name': 'John',
-    'Last Name': 'Doe',
-    'Job Title': 'CTO',
-    'Email': 'john.doe@newcompany.com',
-    'Linkedin URL': 'https://linkedin.com/in/johndoe'
-}
-
-# Call the function to add the new lead
-add_new_lead(new_lead)
-
-
-# Function to delete a lead from the database
-def delete_lead(lead_id):
+# Function to delete multiple leads based on a list of lead IDs
+def delete_leads(lead_id_list):
     leads_collection = connect_to_mongodb()
     
-    # Delete the lead with the specified ID
-    result = leads_collection.delete_one({'_id': ObjectId(lead_id)})
+    # Convert all lead IDs to ObjectId format
+    object_id_list = [ObjectId(lead_id) for lead_id in lead_id_list]
     
-    # Check if a document was deleted
+    # Delete the leads with the specified IDs using $in operator
+    result = leads_collection.delete_many({'_id': {'$in': object_id_list}})
+    
+    # Check how many documents were deleted
     if result.deleted_count > 0:
-        print(f"Lead with ID {lead_id} has been deleted.")
+        print(f"{result.deleted_count} leads have been deleted.")
     else:
-        print(f"No lead found with ID {lead_id}.")
+        print("No leads found with the specified IDs.")
 
-# Example lead ID to delete
-lead_id_to_delete = '66ebcdb633778cd97ba4c365'  # Replace with the actual ID of the lead to delete
-
-# Call the function to delete the lead
-delete_lead(lead_id_to_delete)
+# flow of the text exectution
+print(f"The lead list is as follows : {new_lead_list}\n")
+print('\n')
+print("adding the leads to the database!!!\n")
+add_new_leads(new_lead_list)
+print("new leads added!!\n")
+print("fetching the leads from the database for classification\n")
+lead_info = lead_classification_update()
+print("segregating the classified leads into batches\n")
+batches = get_batches(lead_info)
+print("testing the response_generation by passing the leads to the api \n")
+processed_batches = batch_processing(batches)
+print("updating the batches to the database\n")
+update_leads(processed_batches)
+print("giving the user time to check for the update in the database\n")
+time.sleep(60)
+print("deleting the dummy leads from the database\n")
+delete_leads(processed_batches)
+print("leads have been deleted from the database\n")
+print("the agent is working as expected!!!")
